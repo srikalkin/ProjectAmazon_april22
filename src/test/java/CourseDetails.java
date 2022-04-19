c1db93d904c422deee6779968042d2e7b3c46dfd
srikalkin <srikalkinsriram@gmail.com>
2022-04-19 11:27:55 +0530

    project structure

---
+++ src/test/java/CourseDetails.java
@@ -0,0 +1,26 @@
+
+
+//class creation
+public class CourseDetails {
+//method creation
+	private void javaCourse() {
+		System.out.println("Java Course");
+	}
+	private void pythonCourse() {
+		System.out.println("Python course");
+	}
+	private void seleniumCourse() {
+		System.out.println("Selenium Course");
+	}
+	//Main method creation
+	public static void main(String[] args) {	
+		//object creation
+		//classname objectname=new classname();
+		CourseDetails c=new CourseDetails();
+		//method calling
+		//ObjectName.method
+		c.javaCourse();
+		c.pythonCourse();
+		c.seleniumCourse();
+	}
+}
